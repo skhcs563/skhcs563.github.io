959215a72bb4f7157f2782bfdf0c53c5f8fba76b
skhcs563 <51762551+skhcs563@users.noreply.github.com>
2019-06-13 05:45:00 +0900

    Update key.js

--- key.js
+++ key.js
@@ -1,2 +1,2 @@
-window.__SKYWAY_KEY__ = "dcffd3f8-3ce6-4e88-ab3d-97eca4d871f8"
+window.__SKYWAY_KEY__ = "8b001090-bcaf-4097-9408-c7577c614a32"
 window.__TRANSLATE_URL__ = "https://script.google.com/macros/s/AKfycbw9Ss8FlBn_gwwH8GLMHj8kt03oLOdrSphtyz4O5tBpDqGEBto/exec";
