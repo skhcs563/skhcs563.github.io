d120deeb7353f7531cc83830b50218c0fc73f006
skhcs563 <51762551+skhcs563@users.noreply.github.com>
2019-06-26 10:41:17 +0900

    Update key.js
    nipponseiki.pb110.002

--- key.js
+++ key.js
@@ -1,2 +1,2 @@
 window.__SKYWAY_KEY__ = "8b001090-bcaf-4097-9408-c7577c614a32"
-window.__TRANSLATE_URL__ = "https://script.google.com/macros/s/AKfycbyMrYw0sPwtVGHNWxbaLPljau8mMLeur1xk-1T64z15O9H4330/exec";
+window.__TRANSLATE_URL__ = "https://script.google.com/macros/s/AKfycbwOXCDHqIOHeBuCqVtuiXrtQYLIbHm23UwkKoV-cNYNiAey3qdR/exec";
