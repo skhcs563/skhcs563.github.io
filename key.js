e10551e8564553b8839e78f9118f133ac422035b
skhcs563 <51762551+skhcs563@users.noreply.github.com>
2019-07-01 22:37:08 +0900

    Update key.js

--- key.js
+++ key.js
@@ -1,2 +1,2 @@
 window.__SKYWAY_KEY__ = "8b001090-bcaf-4097-9408-c7577c614a32"
-window.__TRANSLATE_URL__ = "https://script.google.com/macros/s/AKfycbzt6lzmRD-ianSfSM_WIKJKnc3KOUAOyPmZgMDm7MU_MHQKB9M/exec";
+window.__TRANSLATE_URL__ = "https://script.google.com/macros/s/AKfycbweZRcjyKFSWv65tQzH9oAk6vClh9-QyA_RWf3xVxirLDNORAE/exec";
